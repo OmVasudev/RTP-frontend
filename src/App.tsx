696a8d15a9055cc7fd58223c937385516b773f05
Om Vasudev <omvasudev5055@gmail.com>
2024-11-30 13:36:38 +0530

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Login from "../src/components/Login";
-import Register from "../src/components/Register";
-import Editor from "../src/components/Editor";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./pages/auth/Login";
+import PrivateRoutes from "./helpers/routes/PrivateRoutes";
+import Register from "./pages/auth/Register";
+import DocumentHome from "./pages/main/DocumentHome";
+import EditDocument from "./pages/main/EditDocument";
 
-const App: React.FC = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/editor" element={<Editor />} />
-    </Routes>
+    <>
+      <Router>
+        <Navbar />
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/" element={<PrivateRoutes />}>
+            <Route path="/home" element={<DocumentHome />} />
+            <Route path="/edit/:id" element={<EditDocument />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
-};
+}
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,15 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/authContext.jsx";
+import { SupplierProvider } from "./context/supplierContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <SupplierProvider>
+        <App />
+      </SupplierProvider>
+    </AuthProvider>
   </React.StrictMode>,
 );

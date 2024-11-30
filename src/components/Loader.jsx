cf5906248f7e98e4808c2c1305013dd4cb16f61b
Prasanna <gitprasanna@gmail.com>
2024-11-30 14:33:59 +0530

    components

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Loader = () => {
-    return (
-        <div className="spinner-border text-primary my-5" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
+  return (
+    <div className="spinner-border text-primary my-5" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Loader
+export default Loader;
